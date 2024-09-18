001fcada387e0384c1ec59d45e31ca3885b0a045
DISSANAYAKE D.M.C.Y.B <IT24100047@my.sliit.lk>
2024-09-18 08:53:38 +0530

    Add Lab 06

---
+++ Lab 06/IT24100047Lab6Q2A_M2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT24100047Lab6Q2A_M2 {
+    public static void main(String[] args) {
+         //create scanner
+        Scanner get = new Scanner(System.in);
+
+        //declare variables
+        int count = 1;
+
+        //loop
+        while(count  !=11){
+            System.out.print(count+" ");
+            count = count+1;
+        }
+    }
+}
